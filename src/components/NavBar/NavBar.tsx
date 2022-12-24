84dd6ad72291fbb1670fb5d78d5cdce8641c3fe8
Michiel Baert <michiel.baert@student.ap.be>
2022-12-24 11:43:53 +0100

    fix navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import {NavLink} from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/container'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "./NavBar.module.css";
 
